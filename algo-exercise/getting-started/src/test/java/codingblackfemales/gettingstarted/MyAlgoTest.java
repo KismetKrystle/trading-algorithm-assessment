26d7b55b32983139aa2ff4ca8edd5238c07e0a21
Kismet Krystle <kismetaievolution@gmail.com>
2024-12-05 00:54:27 +0800

    added loic for tick2() and added import org.agrona.concurrent.UnsafeBuffer to MyAlgoTest

--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -6,6 +6,8 @@ import codingblackfemales.sotw.marketdata.AskLevel;
 import codingblackfemales.sotw.marketdata.BidLevel;
 import messages.order.Side;
 
+import org.agrona.concurrent.UnsafeBuffer;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -29,7 +31,37 @@ public class MyAlgoTest extends AbstractAlgoTest {
         return new MyAlgoLogic();
     }
 
+    protected UnsafeBuffer createTick2() {
 
+        final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
+        final BookUpdateEncoder encoder = new BookUpdateEncoder();
+    
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
+    
+        // Write the encoded output to the direct buffer
+        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
+    
+        // Set the fields to desired values
+        encoder.venue(Venue.XLON);
+        encoder.instrumentId(123L);
+    
+        encoder.askBookCount(3)
+                .next().price(120L).size(101L) // Higher ask prices
+                .next().price(125L).size(200L)
+                .next().price(130L).size(500L);
+    
+        encoder.bidBookCount(3)
+                .next().price(90L).size(100L)  // Lower bid prices
+                .next().price(85L).size(200L)
+                .next().price(80L).size(300L);
+    
+        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
+        encoder.source(Source.STREAM);
+    
+        return directBuffer;
+    }
+    
     @Test
     public void testDispatchThroughSequencer() throws Exception {
 
@@ -46,16 +78,18 @@ public class MyAlgoTest extends AbstractAlgoTest {
         var order = childOrders.get(0);
         assertEquals(Side.BUY, order.getSide());
         assertEquals(100L, order.getPrice());
-        assertEquals(1, order.getSize());
+        assertEquals(1, order.getQuantity());
     }
 
     @Test
     public void testNoActionWhenThresholdNotMet() throws Exception {
         // Simulate a market data tick with a high highest bid
         send(createTick2());
+       
 
         // Check that no orders were created
         var childOrders = container.getState().getChildOrders();
         assertTrue(childOrders.isEmpty());
     }
+
 }
